43f980996cd162336dec138891fecfc6f9c1589f
Hatchwald <fatkhur139wec@gmail.com>
2024-03-28 12:27:03 +0700

    feat: confiq sequelize feat: add model and migration user

---
+++ config/config.js
@@ -0,0 +1,11 @@
+require("dotenv").config()
+module.exports = {
+    development: {
+        dialect: "mysql",
+        host: process.env.HOST,
+        username: process.env.USERDB,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        port: 3306
+    }
+};
